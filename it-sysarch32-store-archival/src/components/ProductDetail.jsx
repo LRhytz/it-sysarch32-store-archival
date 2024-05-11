9dde7c67b736429552e717fb3ccbdc38384298ab
Rhytz Archival <78730-22215560@SERVER2.DOMAIN>
2024-05-11 12:35:44 +0800

    Rhytz Committed

--- it-sysarch32-store-archival/src/components/ProductDetail.jsx
+++ it-sysarch32-store-archival/src/components/ProductDetail.jsx
@@ -2,10 +2,43 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { db } from '../configs/firebase.js';
 import { doc, getDoc } from 'firebase/firestore';
+import { loadStripe } from '@stripe/stripe-js';
+
+// Load the Stripe.js library with your publishable API key
+const stripePromise = loadStripe('pk_test_51PF6cwGtKecti7APZnyCxO4ZRvkqDkqmNVIW1BHPpFdmif3lhWImV4jcPxZZbFHGzaXeP2NNg64uU2U8vzK9GW7c00kZrjQxSY');
+
 
 function ProductDetail() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
+  const handleClick = async ( productName, price ) => {
+    const stripe = await stripePromise;
+
+    // Send a request to the backend to create a checkout session
+    const response = await fetch('http://localhost:4000/create-checkout-session', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ productName, price }), // Send product name and price to the backend
+    });
+
+    if (response.ok) {
+      // If the request is successful, retrieve the session ID from the response
+      const session = await response.json();
+
+      // Redirect the user to the Stripe Checkout page using the session ID
+      const result = await stripe.redirectToCheckout({ sessionId: session.id });
+
+      if (result.error) {
+        // If there is an error during the redirect, display the error message
+        setError(result.error.message);
+      }
+    } else {
+      // If there is an error creating the checkout session, display an error message
+      setError('Error creating checkout session');
+    }
+  };
 
   useEffect(() => {
     async function fetchProduct() {
@@ -31,10 +64,9 @@ function ProductDetail() {
         <img src={product.product_image} alt="Product" />
         <p>Name: {product.product_name}</p>
         <p>Price: ${product.product_price}</p>
-
         <p>Description: {product.product_description}</p>
         <p>Quantity: {product.product_quantity}</p>
-    
+        <button onClick={() => handleClick(product.product_name,product.product_price*100)}>Checkout</button>
       </div>
     </div>
   );
